47cb3080ec48766c671431516dad26eca8b62ad9
Brad <jbharris@gmail.com>
2022-03-14 13:43:13 -0400

    final product ---- lol maybe

---
+++ js/script.js
@@ -0,0 +1,163 @@
+// document.getElementById("para").onmouseover = function() {mouseOver()};
+// document.getElementById("para").onmouseout = function() {mouseOut()};
+//
+// function mouseOver() {
+//   document.getElementById("para").style.color = "red";
+// }
+//
+// function mouseOut() {
+//   document.getElementById("para").style.color = "white";
+// }
+
+
+
+// first pic fade in
+function test1(){
+  document.getElementById("pic1").setAttribute(
+    "style", "opacity: 0.2; transition: .5s ease;"
+  );
+  document.getElementById("text1").setAttribute(
+    "style", "font-size: 36px; display: block; text-align: center; color: white;"
+  );
+  //first pic fade out
+}
+function out1(){
+  document.getElementById("pic1").setAttribute(
+    "style", "opacity: 1.0; "
+  );
+  document.getElementById("text1").setAttribute(
+    "style", "display: none; "
+  );
+}
+
+//  2nd pic fade in
+function test2(){
+  document.getElementById("pic2").setAttribute(
+    "style", "opacity: 0.2; transition: .5s ease;"
+  );
+  document.getElementById("text2").setAttribute(
+    "style", "font-size: 36px; display: block; text-align: center; color: white;"
+  );
+  // 2nd pic fade out
+}
+function out2(){
+  document.getElementById("pic2").setAttribute(
+    "style", "opacity: 1.0; "
+  );
+  document.getElementById("text2").setAttribute(
+    "style", "display: none; "
+  );
+}
+
+// 3rd pic fade in
+function test3(){
+  document.getElementById("pic3").setAttribute(
+    "style", "opacity: 0.2; transition: .5s ease;"
+  );
+  document.getElementById("text3").setAttribute(
+    "style", "font-size: 36px; display: block; text-align: center; color: white;"
+  );
+  //3rd pic fade out
+}
+function out3(){
+  document.getElementById("pic3").setAttribute(
+    "style", "opacity: 1.0; "
+  );
+  document.getElementById("text3").setAttribute(
+    "style", "display: none; "
+  );
+}
+
+// 4th pic fade in
+function test4(){
+  document.getElementById("pic4").setAttribute(
+    "style", "opacity: 0.2; transition: .5s ease;"
+  );
+  document.getElementById("text4").setAttribute(
+    "style", "font-size: 36px; display: block; text-align: center; color: white;"
+  );
+  //4th pic fade out
+}
+function out4(){
+  document.getElementById("pic4").setAttribute(
+    "style", "opacity: 1.0; "
+  );
+  document.getElementById("text4").setAttribute(
+    "style", "display: none; "
+  );
+}
+// 5th pic fade in
+function test5(){
+  document.getElementById("pic5").setAttribute(
+    "style", "opacity: 0.2; transition: .5s ease;"
+  );
+  document.getElementById("text5").setAttribute(
+    "style", "font-size: 36px; display: block; text-align: center; color: white;"
+  );
+  //5th pic fade out
+}
+function out5(){
+  document.getElementById("pic5").setAttribute(
+    "style", "opacity: 1.0; "
+  );
+  document.getElementById("text5").setAttribute(
+    "style", "display: none; "
+  );
+}
+
+// 6th pic fade in
+function test6(){
+  document.getElementById("pic6").setAttribute(
+    "style", "opacity: 0.2; transition: .5s ease;"
+  );
+  document.getElementById("text6").setAttribute(
+    "style", "font-size: 36px; display: block; text-align: center; color: white;"
+  );
+  //6th pic fade out
+}
+function out6(){
+  document.getElementById("pic6").setAttribute(
+    "style", "opacity: 1.0; "
+  );
+  document.getElementById("text6").setAttribute(
+    "style", "display: none; "
+  );
+}
+
+// 7th pic fade in
+function test7(){
+  document.getElementById("pic7").setAttribute(
+    "style", "opacity: 0.2; transition: .5s ease;"
+  );
+  document.getElementById("text7").setAttribute(
+    "style", "font-size: 36px; display: block; text-align: center; color: white;"
+  );
+  //7th pic fade out
+}
+function out7(){
+  document.getElementById("pic7").setAttribute(
+    "style", "opacity: 1.0; "
+  );
+  document.getElementById("text7").setAttribute(
+    "style", "display: none; "
+  );
+}
+
+// 8th pic fade in
+function test8(){
+  document.getElementById("pic8").setAttribute(
+    "style", "opacity: 0.2; transition: .5s ease;"
+  );
+  document.getElementById("text8").setAttribute(
+    "style", "font-size: 36px; display: block; text-align: center; color: white;"
+  );
+  //8th pic fade out
+}
+function out8(){
+  document.getElementById("pic8").setAttribute(
+    "style", "opacity: 1.0; "
+  );
+  document.getElementById("text8").setAttribute(
+    "style", "display: none; "
+  );
+};
